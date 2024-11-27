7daaa7ed589fc0f45175ead4fab17b214c95271e
Astambar <leuchartjason@gmail.com>
2024-11-27 17:58:10 +0100

    debug spider_produit

--- la_maison/spiders/test.py
+++ la_maison/spiders/test.py
@@ -1,11 +1,12 @@
 import json
 JSON_CATEGORIES = None
 with open('categories.json', 'r') as f:
-    # Comment: pyfile.txt
     JSON_CATEGORIES = json.load(f)
-titre_du_site = JSON_CATEGORIES[0]["titre"]
-dict_categories = JSON_CATEGORIES[0]["categories"]
-for categorie in dict_categories:
-    name_categorie = categorie["nom_cat"]
-    for subcategorie in categorie["sous_categories"]:
-        print(f"{name_categorie}:{subcategorie["nom_ss_cat"]}")
+
+liste_urls = []
+for objet in JSON_CATEGORIES : 
+    liste_urls.append(objet["url"])
+    
+print(len(liste_urls))
+
+print(liste_urls[0])

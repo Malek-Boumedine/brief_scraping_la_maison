8e1ef62c2ca9a56cd35553dca86f56af58f5bcd5
Ubuntu <ubuntu@vps-9ef52d87.vps.ovh.net>
2024-11-28 20:48:45 +0000

    Recherche de doublon dans la base de donné id_produit

--- la_maison/test.py
+++ la_maison/test.py
@@ -1,5 +1,16 @@
 import csv
-
-with open("categories.csv", newline="") as fichier:
-    donnees = csv.DictReader(fichier, delimiter=",")  
-    urls = [row["url"] for row in donnees]  
+def search_id_produit():
+    with open("produits.csv", newline="") as fichier:
+        donnees = csv.DictReader(fichier, delimiter=",")  
+        return [int(row["id_produit "]) for row in donnees]
+product_id_list = search_id_produit()
+product_id_list.sort()
+is_unique = True
+len_product_id = len(product_id_list)
+for i in range(len_product_id-1):
+    if product_id_list[i] == product_id_list[i+1]:
+        is_unique = False
+        print(f"{product_id_list[i]=} valeur identique {product_id_list[i+1]}")
+        break
+if is_unique:
+    print("aucun doublon repérer")
